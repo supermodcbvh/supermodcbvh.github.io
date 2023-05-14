c4353fb2ab627640170effb12017edfe5d4812b0
NRO-Design <132861911+NRO-Design@users.noreply.github.com>
2023-05-14 14:48:00 +0700

    Update ngrok.php

--- Cloud/ngrok.php
+++ Cloud/ngrok.php
@@ -1,18 +1,4 @@
 <?php
-// Determine system architecture
-$system_arch = shell_exec("uname -m");
-
-// Determine Ngrok binary download URL
-if (strpos($system_arch, 'arm') !== false) {
-    $ngrok_url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip';
-} else if (strpos($system_arch, 'aarch64') !== false) {
-    $ngrok_url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip';
-} else if (strpos($system_arch, 'x86_64') !== false) {
-    $ngrok_url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip';
-} else {
-    die("Error: Unsupported system architecture\n");
-}
-
 // Prompt user to enter Ngrok auth token
 echo "Enter your Ngrok Auth Token: ";
 $authToken = trim(fgets(STDIN));
@@ -21,13 +7,9 @@ $authToken = trim(fgets(STDIN));
 echo "Enter the TCP Port you want to open (maximum 65535): ";
 $tcpPort = trim(fgets(STDIN));
 
-// Download Ngrok binary
-shell_exec("curl -L $ngrok_url -o ngrok.zip");
-
-// Extract Ngrok binary
+// Download and extract Ngrok binary
+shell_exec("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip");
 shell_exec("unzip -qq ngrok.zip");
-
-// Remove Ngrok binary archive
 shell_exec("rm ngrok.zip");
 
 // Set execute permissions on the Ngrok binary
@@ -35,8 +17,7 @@ shell_exec("chmod +x ./ngrok");
 
 // Check if ngrok is executable and can run on this system
 $file_type = shell_exec("file ./ngrok");
-$is_executable = strpos($file_type, 'executable') !== false;
-if (!$is_executable) {
+if (strpos($file_type, 'executable') === false) {
     die("Error: Ngrok is not executable\n");
 }
 
@@ -49,3 +30,4 @@ if ($is_64bit && strpos($system_arch, 'x86_64') === false) {
 // Connect to Ngrok with auth token and open TCP port
 $ngrokCommand = "./ngrok authtoken $authToken && ./ngrok tcp $tcpPort";
 passthru($ngrokCommand);
+?>
