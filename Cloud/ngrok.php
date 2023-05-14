74b5d3b8b7ece61daf4f84c271aca9d95231b4ed
NRO-Design <132861911+NRO-Design@users.noreply.github.com>
2023-05-14 14:57:00 +0700

    Update ngrok.php

--- Cloud/ngrok.php
+++ Cloud/ngrok.php
@@ -1,7 +1,15 @@
-// Determine system architecture
+<?php
+// Prompt user to enter Ngrok auth token
+echo "Enter your Ngrok Auth Token: ";
+$authToken = trim(fgets(STDIN));
+
+// Prompt user to enter the TCP port to open
+echo "Enter the TCP Port you want to open (maximum 65535): ";
+$tcpPort = trim(fgets(STDIN));
+
+// Check the system architecture first
 $system_arch = shell_exec("uname -m");
 
-// Determine Ngrok binary download URL
 if (strpos($system_arch, 'arm') !== false) {
     $ngrok_url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip';
 } else if (strpos($system_arch, 'aarch64') !== false) {
@@ -12,35 +20,26 @@ if (strpos($system_arch, 'arm') !== false) {
     die("Error: Unsupported system architecture\n");
 }
 
-// Prompt user to enter Ngrok auth token
-echo "Enter your Ngrok Auth Token: ";
-$authToken = trim(fgets(STDIN));
+// Download ngrok
+$ngrokZip = "ngrok.zip";
+file_put_contents($ngrokZip, file_get_contents($ngrok_url));
 
-// Prompt user to enter the TCP port to open
-echo "Enter the TCP Port you want to open (maximum 65535): ";
-$tcpPort = trim(fgets(STDIN));
-
-// Download Ngrok binary
-shell_exec("curl -L $ngrok_url -o ngrok.zip");
-
-// Extract Ngrok binary
-shell_exec("unzip -qq ngrok.zip");
-
-// Remove Ngrok binary archive
-shell_exec("rm ngrok.zip");
+// Extract ngrok and remove the zip file
+shell_exec("unzip -qq $ngrokZip");
+shell_exec("rm $ngrokZip");
 
 // Set execute permissions on the Ngrok binary
 shell_exec("chmod +x ./ngrok");
 
 // Check if ngrok is executable and can run on this system
 $file_type = shell_exec("file ./ngrok");
-$is_executable = strpos((string)$file_type, 'executable') !== false;
+$is_executable = strpos($file_type, 'executable') !== false;
 if (!$is_executable) {
     die("Error: Ngrok is not executable\n");
 }
 
 // Check if ngrok is 64-bit or not
-$is_64bit = strpos((string)$file_type, '64-bit') !== false;
+$is_64bit = strpos($file_type, '64-bit') !== false;
 if ($is_64bit && strpos($system_arch, 'x86_64') === false) {
     die("Error: Ngrok is 64-bit but your system is not\n");
 }
@@ -48,3 +47,5 @@ if ($is_64bit && strpos($system_arch, 'x86_64') === false) {
 // Connect to Ngrok with auth token and open TCP port
 $ngrokCommand = "./ngrok authtoken $authToken && ./ngrok tcp $tcpPort";
 passthru($ngrokCommand);
+
+?>
