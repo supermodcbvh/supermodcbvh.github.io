0adde8974256a362564f315be07794f0a4192464
NRO-Design <132861911+NRO-Design@users.noreply.github.com>
2023-05-14 15:10:44 +0700

    Update ngrok.php

--- Cloud/ngrok.php
+++ Cloud/ngrok.php
@@ -1,48 +1,25 @@
 <?php
-// Prompt user to enter Ngrok auth token
 echo "Enter your Ngrok Auth Token: ";
 $authToken = trim(fgets(STDIN));
 
-// Prompt user to enter the TCP port to open
 echo "Enter the TCP Port you want to open (maximum 65535): ";
 $tcpPort = trim(fgets(STDIN));
 
-// Check system architecture and set Ngrok download URL
-$system_arch = shell_exec("uname -m");
+shell_exec("wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3.3.0-linux-amd64.tar.gz -O ngrok.tar.gz && tar -xvf ngrok.tar.gz && rm ngrok.tar.gz");
 
-if (strpos($system_arch, 'arm') !== false) {
-    $ngrok_url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip';
-} else if (strpos($system_arch, 'aarch64') !== false) {
-    $ngrok_url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip';
-} else if (strpos($system_arch, 'x86_64') !== false) {
-    $ngrok_url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip';
-} else {
-    die("Error: Unsupported system architecture\n");
-}
-
-// Download and extract Ngrok
-shell_exec("wget -q $ngrok_url -O ngrok.zip");
-shell_exec("unzip -qq ngrok.zip");
-shell_exec("rm ngrok.zip");
-
-// Set execute permissions on the Ngrok binary
 shell_exec("chmod +x ./ngrok");
-
-// Check if ngrok is executable and can run on this system
 $file_type = shell_exec("file ./ngrok");
 $is_executable = strpos($file_type, 'executable') !== false;
 if (!$is_executable) {
     die("Error: Ngrok is not executable\n");
 }
 
-// Check if ngrok is 64-bit or not
+$system_arch = shell_exec("uname -m");
 $is_64bit = strpos($file_type, '64-bit') !== false;
 if ($is_64bit && strpos($system_arch, 'x86_64') === false) {
     die("Error: Ngrok is 64-bit but your system is not\n");
 }
 
-// Connect to Ngrok with auth token and open TCP port
-$ngrokCommand = "./ngrok authtoken $authToken && ./ngrok tcp $tcpPort";
+$ngrokCommand = "./ngrok authtoken $authToken && ./ngrok tcp --region=jp $tcpPort";
 passthru($ngrokCommand);
-
 ?>
