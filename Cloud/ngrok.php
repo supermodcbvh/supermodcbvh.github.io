993fc83aa15a63f69dc5a97e2db23aef1bd7f514
NRO-Design <132861911+NRO-Design@users.noreply.github.com>
2023-05-14 15:19:00 +0700

    Update ngrok.php

--- Cloud/ngrok.php
+++ Cloud/ngrok.php
@@ -5,21 +5,31 @@ $authToken = trim(fgets(STDIN));
 echo "Enter the TCP Port you want to open (maximum 65535): ";
 $tcpPort = trim(fgets(STDIN));
 
-shell_exec("wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3.3.0-linux-amd64.tar.gz -O ngrok.tar.gz && tar -xvf ngrok.tar.gz && rm ngrok.tar.gz");
+// Determine system architecture
+$system_arch = shell_exec("uname -m");
+if (strpos($system_arch, 'x86_64') !== false) {
+    // 64-bit system
+    $ngrok_download_url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip';
+} elseif (strpos($system_arch, 'arm') !== false) {
+    // ARM-based system
+    $ngrok_download_url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip';
+} else {
+    // Unsupported system
+    die("Error: Unsupported system architecture ($system_arch)\n");
+}
 
+// Download and extract Ngrok
+shell_exec("wget $ngrok_download_url -O ngrok.zip && unzip -qq ngrok.zip && rm ngrok.zip");
 shell_exec("chmod +x ./ngrok");
+
+// Check if Ngrok is executable
 $file_type = shell_exec("file ./ngrok");
 $is_executable = strpos($file_type, 'executable') !== false;
 if (!$is_executable) {
     die("Error: Ngrok is not executable\n");
 }
 
-$system_arch = shell_exec("uname -m");
-$is_64bit = strpos($file_type, '64-bit') !== false;
-if ($is_64bit && strpos($system_arch, 'x86_64') === false) {
-    die("Error: Ngrok is 64-bit but your system is not\n");
-}
-
+// Connect to Ngrok with auth token and open TCP port
 $ngrokCommand = "./ngrok authtoken $authToken && ./ngrok tcp --region=jp $tcpPort";
 passthru($ngrokCommand);
 ?>
