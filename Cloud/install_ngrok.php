7666ef4519651a3b0e6e0695d4457c98f6b28297
NRO-Design <132861911+NRO-Design@users.noreply.github.com>
2023-05-14 11:07:59 +0700

    Update install_ngrok.php

--- Cloud/install_ngrok.php
+++ Cloud/install_ngrok.php
@@ -3,9 +3,6 @@ $token = readline("Enter your Ngrok Auth Token: ");
 
 $port = readline("Enter the TCP Port you want to open: ");
 
-// Update and upgrade packages
-system("apt update && apt upgrade");
-
 // Download ngrok
 system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip");
 
