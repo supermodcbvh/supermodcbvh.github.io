e60a8f8655a6be08cdcd46a83716ac46467628a8
NRO-Design <132861911+NRO-Design@users.noreply.github.com>
2023-05-14 13:12:39 +0700

    Update ngrok.php

--- Cloud/ngrok.php
+++ Cloud/ngrok.php
@@ -14,6 +14,20 @@ shell_exec("rm ngrok.zip");
 // Set execute permissions on the Ngrok binary
 shell_exec("chmod +x ./ngrok");
 
+// Check if ngrok is executable and can run on this system
+$file_type = shell_exec("file ./ngrok");
+$is_executable = strpos($file_type, 'executable') !== false;
+if (!$is_executable) {
+    die("Error: Ngrok is not executable\n");
+}
+
+// Check if ngrok is 64-bit or not
+$system_arch = shell_exec("uname -m");
+$is_64bit = strpos($file_type, '64-bit') !== false;
+if ($is_64bit && strpos($system_arch, 'x86_64') === false) {
+    die("Error: Ngrok is 64-bit but your system is not\n");
+}
+
 // Connect to Ngrok with auth token and open TCP port
 $ngrokCommand = "./ngrok authtoken $authToken && ./ngrok tcp $tcpPort";
 $output = shell_exec($ngrokCommand);
